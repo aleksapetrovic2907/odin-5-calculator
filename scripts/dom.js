e00127dbfd2d237eb35f4ad93b4e64110045e6c5
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-27 14:06:19 +0100

    cache relevant nodes on DOMContentLoaded

---
+++ scripts/dom.js
@@ -0,0 +1,11 @@
+let display;
+let digitsContainer;
+let operationsContainer;
+
+document.addEventListener("DOMContentLoaded", initialize);
+
+function initialize() {
+    display = document.querySelector("#display");
+    digitsContainer = document.querySelector("#digits-container");
+    operationsContainer = document.querySelector("#operations-container");
+}
